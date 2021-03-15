4bcdc663945c030199808d4d01c9e1d1fa773f40
KateIsabelle <kate.davis887@gmail.com>
2021-03-15 19:18:03 +0000

    updated dependencies

--- express_server.js
+++ express_server.js
@@ -4,38 +4,3 @@ const app = express();
 app.set('view engine', 'ejs');
 const PORT = 8080; 
 
-//use res.render to load up an ejs view file 
-//index page
-app.get('/', function(req, res) {
-  res.render('pages/index');
-})
-//about page
-app.get('/about', function() {
-  res.render('pages/about');
-})
-
-app.listen(PORT);
-
-
-
-// const urlDatabase = {
-//   "b2xVn2": "http://www.lighthouselabs.ca",
-//   "9sm5xK": "http://www.google.com"
-// };
-
-// app.get("/", (req, res) => {
-//   res.send("Hello!");
-// });
-
-// app.get("/urls.json", (req, res) => {
-//   res.json(urlDatabase);
-// });
-
-// app.get("/hello", (req, res) => {
-//   res.send("<html><body>Hello <b>World</b></body></html>\n");
-// });
-
-// app.listen(PORT, () => {
-//   console.log(`Example app listening on port ${PORT}!`);
-// });
- 
