5f14cfb50e592ae289d2cc222c3575d05f786cb6
KateIsabelle <kate.davis887@gmail.com>
2021-03-15 19:24:44 +0000

    erase tutorial code, add back starter code and add new route handler for '/urls' and use res.render() to pass URL data to template

--- express_server.js
+++ express_server.js
@@ -4,3 +4,21 @@ const app = express();
 app.set('view engine', 'ejs');
 const PORT = 8080; 
 
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+})
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
