8ac52a22bd268846da3f018b81eca3a489998629
KateIsabelle <kate.davis887@gmail.com>
2021-03-15 18:25:56 +0000

    creat views folder, new ejs files, and update express_server to access those files with res.render (following along with ejs tutorial)

--- express_server.js
+++ express_server.js
@@ -1,25 +1,41 @@
 const express = require("express");
 const app = express();
+//set the view engine to ejs
+app.set('view engine', 'ejs');
 const PORT = 8080; 
 
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
+//use res.render to load up an ejs view file 
+//index page
+app.get('/', function(req, res) {
+  res.render('pages/index');
+})
+//about page
+app.get('/about', function() {
+  res.render('pages/about');
+})
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
+app.listen(PORT);
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
+// const urlDatabase = {
+//   "b2xVn2": "http://www.lighthouselabs.ca",
+//   "9sm5xK": "http://www.google.com"
+// };
+
+// app.get("/", (req, res) => {
+//   res.send("Hello!");
+// });
+
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
+
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`Example app listening on port ${PORT}!`);
+// });
  
