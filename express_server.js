925cc3542a049e3992e1854f786df357dfbea652
KateIsabelle <kate.davis887@gmail.com>
2021-03-16 18:44:18 +0000

    add pseudocode & post request that deletes url from database when delete button is pushed

--- express_server.js
+++ express_server.js
@@ -11,16 +11,18 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
+//helper function
 const generateRandomString = function() {
   let string = Math.random().toString(36).slice(7);
   return string;
 };
 
+//end points or routes:
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
-
+//
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
@@ -30,6 +32,9 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+//for urls ending in a /shortURL, render short and long urls
+//as key-value pairs inside template variables object
+//and send to be used in urls_show document
 app.get("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL; // grab the thing after the colon above
   const longURL = urlDatabase[shortURL];
@@ -37,17 +42,27 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-app.post("/urls", (req, res) => {
-  let shorty = generateRandomString();
-  urlDatabase[shorty] = req.body.longURL;
-  res.redirect(`/urls/${shorty}`);         // Respond with 'Ok' (we will replace this)
-});
-
+//tied to href anchor in urls_show url short url link
+//upon click, redirects to matching long url
 app.get("/u/:shortURL", (req, res) => {
   const shorty = req.params.shortURL;
   const longURL = urlDatabase[shorty];
   res.redirect(longURL);
 });
+
+//redirects up to app.get("urls/:shortURL")
+app.post("/urls", (req, res) => {
+  let shorty = generateRandomString();
+  urlDatabase[shorty] = req.body.longURL;
+  res.redirect(`/urls/${shorty}`);    
+});
+
+//tied to delete button in urls_index
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls")
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
