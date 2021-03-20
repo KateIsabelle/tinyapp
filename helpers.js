8e4bcf2d398b8cfb82cfba6c32152a45ae44c461
KateIsabelle <kate.davis887@gmail.com>
2021-03-20 17:00:14 +0000

    add pseudocode to explain helper functions

--- helpers.js
+++ helpers.js
@@ -1,5 +1,8 @@
 //helper functions:
 
+//function takes in user id and url database,
+//and returns an object containing just the url objects 
+//that belong to specific user
 const urlsForUser = function (id, database) {
   const urls = {};
   for (let shortURL in database) {
@@ -10,11 +13,16 @@ const urlsForUser = function (id, database) {
   return urls;
 }
 
+//function generates random 6-character string, 
+//to be used for short url or user id
 const generateRandomString = function () {
   let string = Math.random().toString(36).slice(2, 8);
   return string;
 };
 
+//function takes in an email, loops through user database, 
+//and returns user object that matches email
+//if no match, returns undefined
 const getUserByEmail = function (email, database) {
   for (let userId in database) {
     if (database[userId].email === email) {
